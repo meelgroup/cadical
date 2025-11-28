1d1583f8378001714057499a8b769bbc28114e5d
Mate Soos <soos.mate@gmail.com>
2025-11-28 21:01:51 +0100

    Better debug setup

--- build_debug.sh
+++ build_debug.sh
@@ -2,5 +2,5 @@
 rm -rf build/libcadical.*
 rm -f compile_commands.json
 make clean
-CXXFLAGS=-fPIC ./configure --no-contracts --no-tracing --debug
+CXXFLAGS=-fPIC ./configure --no-contracts --no-tracing --debug -ggdb3
 bear -- make -j12
