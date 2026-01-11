16dde5487287b349d19eb9f16642a797e38ca34f
Mate Soos <soos.mate@gmail.com>
2026-01-11 15:27:45 +0100

    Add --fp for frame pointer

--- build_norm.sh
+++ build_norm.sh
@@ -2,5 +2,5 @@
 rm -rf build/libcadical.*
 rm -f compile_commands.json
 make clean
-CXXFLAGS=-fPIC ./configure --no-contracts --no-tracing
+CXXFLAGS=-fPIC ./configure --no-contracts --no-tracing --fp
 bear -- make -j12
