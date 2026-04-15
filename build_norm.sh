088c57bdcf98bcae0923c2089d0e26bffd51ac30
Mate Soos <soos.mate@gmail.com>
2026-04-15 12:02:24 +0200

    Update

--- build_norm.sh
+++ build_norm.sh
@@ -12,6 +12,6 @@ rm -rf CM*
 rm -rf cmake*
 rm -rf deps
 rm -rf _deps
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
-cmake --build build -j$(nproc)
-cmake --install build
+cmake -S .. -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
+cmake --build . -j$(nproc)
+cmake --install .
