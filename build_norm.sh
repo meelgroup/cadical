3b31f42eeb09fd218bc0bde32fbebd5d8cb84337
Mate Soos <soos.mate@gmail.com>
2026-04-15 12:09:16 +0200

    Update build

--- build_norm.sh
+++ build_norm.sh
@@ -12,6 +12,6 @@ rm -rf CM*
 rm -rf cmake*
 rm -rf deps
 rm -rf _deps
-cmake -S .. -B . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
+cmake -S .. -B . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(pwd)"
 cmake --build . -j$(nproc)
 cmake --install .
--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-cmake -S .. -B . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
+cmake -S .. -B . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="$(pwd)"
 cmake --build . -j$(nproc)
 cmake --install .
