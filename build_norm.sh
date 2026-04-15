5d671d5034ac3dfcd2a70ac193f94071a6339b54
Mate Soos <soos.mate@gmail.com>
2026-04-15 11:44:06 +0200

    Update build

--- build_norm.sh
+++ build_norm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
-rm -rf build
+rm -rf .cmake
+rm -rf lib*
+rm -rf Test*
+rm -rf tests*
+rm -rf include
+rm -rf tests
+rm -rf approxmc*
+rm -rf apx-src
+rm -rf CM*
+rm -rf cmake*
+rm -rf deps
+rm -rf _deps
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
 cmake --build build -j$(nproc)
 cmake --install build
