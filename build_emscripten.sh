3e2f46a0116b70364370f59205a5f4187390a62a
Mate Soos <soos.mate@gmail.com>
2026-04-17 09:07:52 +0200

    Fix build

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -15,7 +15,10 @@ rm -rf utils
 rm -rf Make*
 rm -rf deps
 rm -rf _deps
-emcmake cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="${EMINSTALL}" -DENABLE_TESTING=OFF ..
+emcmake cmake -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_INSTALL_PREFIX="${EMINSTALL}"  \
+  -DENABLE_TESTING=OFF ..
 emmake make -j$(nproc)
 emmake make install
 # cp cadiback.wasm "${SAT_DIR}/cadiback/html"
