ade472d3dba145fd53c19d486f5916b6259449c6
Mate Soos <soos.mate@gmail.com>
2026-05-24 10:29:26 +0200

    Adding notest

---
+++ build_notest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+rm -rf .cmake
+rm -rf lib*
+rm -rf Test*
+rm -rf tests*
+rm -rf include
+rm -rf tests
+rm -rf approxmc*
+rm -rf apx-src
+rm -rf CM*
+rm -rf cmake*
+rm -rf deps
+rm -rf _deps
+cmake -S .. -B . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX="$(pwd)"
+cmake --build . -j$(nproc)
+cmake --install .
